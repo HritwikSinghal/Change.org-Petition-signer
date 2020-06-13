bf1701a70e1a69d12793ced287610fac5c0d6a12
Hritwik <hritwiksinghal@outlook.in>
2020-06-13 10:42:07 +0530

    rewrite, faster, linux support, bot detection bypass

--- petition.py
+++ petition.py
@@ -1,216 +1,144 @@
-import json
+import os
+import platform
 import random
-import re
-import traceback
 import time
+import traceback
 
-import requests
-from bs4 import BeautifulSoup as beautifulsoup
-
+from faker import Faker
 from selenium import webdriver
-from selenium.webdriver.common.proxy import Proxy
-from selenium.webdriver.common.proxy import ProxyType
 
 
-def print_list(my_list):
-    print('---------------------')
-    for x in my_list:
-        print(x)
-        # print('"' + x + '"' + ',')
-    print('---------------------')
+# def retrieveNames(url, file_name):
+#     user_agent = {
+#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'
+#     }
+#
+#     res = requests.get(url, headers=user_agent)
+#     soup = beautifulsoup(res.content, 'html5lib')
+#
+#     with open(file_name, 'w+', encoding='utf-8') as x:
+#         for line in list(soup.find_all('h3')):
+#             line = str(line).strip('<h3>')
+#             line = str(line).strip('</h3>')
+#
+#             line = re.sub('\d+. ', '', line)
+#             line = line.strip(':')
+#             line = line.strip(". ")
+#             x.writelines(str(line) + '\n')
+
+# def getNames():
+#     last_name_url = 'https://www.momjunction.com/articles/popular-indian-last-names-for-your-baby_00334734/'
+#     first_name_url = 'https://www.momjunction.com/articles/indian-baby-boy-names-with-meanings_00349318/'
+#     retrieveNames(last_name_url, 'last_name')
+#     retrieveNames(first_name_url, 'first_name')
 
 
-def retrieveNames(url, file_name):
-    user_agent = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'
-    }
+def sign(browser):
+    fname = Faker().first_name()
+    lname = Faker().last_name()
+    randemail = fname + lname + str(random.randint(0, 50000)) + '@gmail.com'
 
-    res = requests.get(url, headers=user_agent)
-    soup = beautifulsoup(res.content, 'html5lib')
+    print("Using: ", fname, lname, randemail)
 
-    with open(file_name, 'w+', encoding='utf-8') as x:
-        for line in list(soup.find_all('h3')):
-            line = str(line).strip('<h3>')
-            line = str(line).strip('</h3>')
+    fnamebox = browser.find_element_by_name('firstName')
+    lnamebox = browser.find_element_by_name("lastName")
+    emailbox = browser.find_element_by_name("email")
+    publiccheck = browser.find_element_by_name("public")
 
-            line = re.sub('\d+. ', '', line)
-            line = line.strip(':')
-            line = line.strip(". ")
-            x.writelines(str(line) + '\n')
+    # This will not work
+    # fnamebox.send_keys(fname)
+    # time.sleep(1)
+    # lnamebox.send_keys(lname)
+    # time.sleep(1)
+    # emailbox.send_keys(randemail)
+    # time.sleep(1)
+    # publiccheck.click()
+    # time.sleep(1)
+    # emailbox.submit()
+    # time.sleep(1)
 
+    # this will work
+    for k in fname:
+        fnamebox.send_keys(k)
+        # time.sleep(.1)
 
-def fate_proxy():
-    resp = requests.get('https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list')
-    a = ((resp.text).split('\n'))
-    p_list = []
-    for i in a:
-        try:
-            p_list.append(json.loads(i))
-        except Exception as e:
-            continue
-    np_list = []
+    for k in lname:
+        lnamebox.send_keys(k)
+        # time.sleep(.1)
 
-    countries = ["AE",
-                 "AR",
-                 "AU",
-                 "AZ",
-                 "BD",
-                 "BG",
-                 "BO",
-                 "BR",
-                 "CA",
-                 "CL",
-                 "CN",
-                 "CO",
-                 "CR",
-                 "CZ",
-                 "DE",
-                 "EC",
-                 "FR",
-                 "GB",
-                 "GE",
-                 "GH",
-                 "GN",
-                 "HK",
-                 "ID",
-                 "IL",
-                 "IN",
-                 "IQ",
-                 "JP"
-                 ]
-    for i in p_list:
-        if i['country'] in countries:
-            # print(i['country'])
-            np_list.append(i)
-        # np_list.append(i)
-    proxy = []
-    fast_proxy = sorted(np_list, key=lambda k: k['response_time'])
-    for p in fast_proxy:
-        proxy.append(str(p['host']) + ':' + str(p['port']))
-    return proxy
+    for k in randemail:
+        emailbox.send_keys(k)
+        # time.sleep(.1)
+
+    publiccheck.click()
+    emailbox.submit()
+    time.sleep(3)
+    browser.delete_all_cookies()
 
 
-def getNames():
-    last_name_url = 'https://www.momjunction.com/articles/popular-indian-last-names-for-your-baby_00334734/'
-    first_name_url = 'https://www.momjunction.com/articles/indian-baby-boy-names-with-meanings_00349318/'
-    retrieveNames(last_name_url, 'last_name')
-    retrieveNames(first_name_url, 'first_name')
+def start(test=0, pnum=0):
+    print("""
+      ____  _             ____        _
+     / ___|(_) __ _ _ __ | __ )  ___ | |_
+     \___ \| |/ _` | '_ \|  _ \ / _ \| __|
+      ___) | | (_| | | | | |_) | (_) | |_
+     |____/|_|\__, |_| |_|____/ \___/ \__|
+              |___/
+    """)
+    print("Starting...")
 
-
-def sign(first_names, last_names, proxies, use_proxy, url):
-    if use_proxy == 1:
-        for proxy in proxies:
-            fname = random.choice(first_names).strip()
-            lname = random.choice(last_names).strip()
-            email = fname + lname + str(random.randint(0, 220000)) + '@gmail.com'
-
-            data = {
-                'firstName': fname,
-                'lastName': lname,
-                'email': email
-            }
-
-            # random.shuffle(proxies)
-
-            firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
-            firefox_capabilities['marionette'] = True
-
-            firefox_capabilities['proxy'] = {
-                "proxyType": "MANUAL",
-                "httpProxy": proxy,
-                "ftpProxy": proxy,
-                "sslProxy": proxy
-            }
-            browser = webdriver.Firefox(capabilities=firefox_capabilities)
-
-            try:
-                # proxy = '45.236.88.42:8880'
-                print('Using Proxy: ', proxy)
-                browser.set_page_load_timeout(90)
-
-                browser.get(url)
-                for key in data:
-                    a = browser.find_element_by_name(key)
-                    a.send_keys(data[key])
-
-                try:
-                    b = browser.find_element_by_name('marketingCommsConsent.consentGiven')
-                    b.click()
-                    b.submit()
-                except:
-                    a.submit()
-
-                print("Signed by: ", fname, lname)
-                print()
-                # x = input()
-
-                time.sleep(3)
-                browser.quit()
-
-            except:
-                browser.quit()
-                # traceback.print_exc()
-                print('skipping this proxy')
-                continue
+    if not test:
+        url = input("Enter url: ")
     else:
-        fname = random.choice(first_names).strip()
-        lname = random.choice(last_names).strip()
-        email = fname + lname + str(random.randint(0, 220000)) + '@gmail.com'
+        url = 'https://www.change.org/p/realme-mobiles-release-the-flashtool-for-realme-devices'
 
-        data = {
-            'firstName': fname,
-            'lastName': lname,
-            'email': email
-        }
+    os_name = platform.system()
+    os_arch, t = platform.architecture()
 
-        
-        # url = 'https://www.change.org/p/free-nazanin-ratcliffe?source_location=discover_feed'
+    if os_name == 'Linux':
+        if os_arch == "64bit":
+            loc = './geckodriver_L64'
+        else:
+            loc = './geckodriver_L32'
+    if os_name == "Windows":
+        if os_arch == "64bit":
+            loc = './geckodriver_W64'
+        else:
+            loc = './geckodriver_W32'
 
-        # random.shuffle(proxies)
-        browser = webdriver.Firefox()
+    # these are the workarounds for not getting flagged as bot
+    # UA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0"
+    # profile = webdriver.FirefoxProfile('/home/hritwik/.mozilla/firefox/zufq32k3.New')
+    profile = webdriver.FirefoxProfile()
+    # profile.set_preference("general.useragent.override", UA)
+    profile.set_preference("dom.webdriver.enabled", False)
+    profile.set_preference('useAutomationExtension', False)  # this is must
+    profile.set_preference('devtools.jsonview.enabled', False)
+    profile.update_preferences()
 
+    browser = webdriver.Firefox(profile, executable_path=loc)
+
+    for _ in range(1, 200):
         try:
-            browser.set_page_load_timeout(30)
             browser.get(url)
-            for key in data:
-                a = browser.find_element_by_name(key)
-                a.send_keys(data[key])
-
-            try:
-                b = browser.find_element_by_name('marketingCommsConsent.consentGiven')
-                b.click()
-                b.submit()
-            except:
-                a.submit()
-
-            print("filled by: ", fname, lname)
-            time.sleep(3)
-            browser.quit()
-
+            sign(browser)
         except:
-            browser.quit()
-            return
+            if test:
+                traceback.print_exc()
+
+            browser.delete_all_cookies()
+            print("Error filling form. Refreshing...")
 
 
-def start():
-    first_names = open('first_name', 'r').readlines()
-    last_names = open('last_name', 'r').readlines()
-    proxies = fate_proxy()
-    
-    up = int(input("Do you want to use proxies?.\n"
-              "Only use proxies when your normal connection is blocked\n"
-              "since proxies are very slow.\n"
-              "1== Yes, 0 == NO\n"))
+if os.path.isfile('./test_bit'):
+    test = 1
+else:
+    test = 0
 
-    # url = 'https://www.change.org/p/realme-mobiles-release-the-flashtool-for-realme-devices'
-    url = input('Enter change.org url to sign')
-    
-    for x in range(200):
-        try:
-            sign(first_names, last_names, proxies, up, url)
-        except:
-            print("timeout")
-            continue
-
-
-start()
+try:
+    start(test=test)
+except:
+    if test:
+        traceback.print_exc()
+    print("Exiting...")
+    exit(0)
